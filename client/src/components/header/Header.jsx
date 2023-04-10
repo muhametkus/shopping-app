fbf50c60794729b9ed7de7974d9751e87447d7b8
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2023-04-10 05:08:06 +0300

    Header responsive hale getirildi.

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SearchOutlined,HomeOutlined,ShoppingCartOutlined,CopyOutlined,UserOutlined,BarChartOutlined,LogoutOutlined  } from '@ant-design/icons';
-import { Input } from 'antd';
+import { Badge, Input } from 'antd';
 
 const Header = () => {
   return (
@@ -9,41 +9,56 @@ const Header = () => {
       <div className="logo">
         <a href="/"><h2 className='text-2xl font-bold md:text-4xl'>LOGO</h2></a>
       </div>
-      <div className="header-search flex-1">
+      <div className="header-search flex-1 flex justify-center">
       <Input size="large" placeholder="Ürün Ara..." prefix={<SearchOutlined />} className='rounded-full max-w-[800px]'/>
       </div>
-      <div className="menu-links flex justify-between items-center gap-8">
-        <a href={"/"} className='menu-link flex flex-col'>
+      <div className="menu-links flex justify-between items-center gap-7 md:static fixed bottom-0 md:w-auto w-screen bg-white md:bg-transparent left-0 md:border-t-0 border-t md:px-0 px-4 py-1">
+        <a href={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>
             <HomeOutlined className='md:text-2xl text-xl'/>
             <span className='md:text-xs text-[10px]'>Anasayfa</span>          
         </a>
 
-        <a href={"/"} className='menu-link flex flex-col'>
+        <Badge count={5} offset={[0,6]} className='md:flex hidden'>
+        <a href={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>
             <ShoppingCartOutlined className='md:text-2xl text-xl'/>
             <span className='md:text-xs text-[10px]'>Sepet</span>          
         </a>
 
-        <a href={"/"} className='menu-link flex flex-col'>
+
+
+        </Badge>
+
+        <a href={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>
             <CopyOutlined className='md:text-2xl text-xl'/>
             <span className='md:text-xs text-[10px]'>Faturalar</span>          
         </a>
 
-        <a href={"/"} className='menu-link flex flex-col'>
+        <a href={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>
             <UserOutlined className='md:text-2xl text-xl'/>
             <span className='md:text-xs text-[10px]'>Müşteriler</span>          
         </a>
 
-        <a href={"/"} className='menu-link flex flex-col'>
+        <a href={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>
             <BarChartOutlined className='md:text-2xl text-xl'/>
             <span className='md:text-xs text-[10px]'>İstatistikler</span>          
         </a>
 
-        <a href={"/"} className='menu-link flex flex-col'>
+        <a href={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>
             <LogoutOutlined className='md:text-2xl text-xl'/>
             <span className='md:text-xs text-[10px]'>Çıkış</span>          
         </a>
        
       </div>
+
+      <Badge count={5} offset={[0,6]} className='md:hidden flex'>
+        <a href={"/"} className='menu-link flex flex-col hover:text-[#40a9ff] transition-all'>
+            <ShoppingCartOutlined className='text-2xl'/>
+            <span className='md:text-xs text-[10px]'>Sepet</span>          
+        </a>
+
+
+
+        </Badge>
       
       </header>
     </div>
