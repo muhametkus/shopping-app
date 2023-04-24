bd2a4beb0dc53c1128be9744f2f3f7eb1bca65e5
muhametkus <muhametkus@gmail.com>
2023-04-24 13:59:03 +0300

    CartTotals componenti tamamlandı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,24 @@
+import CartTotals from "./components/cart/CartTotals";
 import Categories from "./components/categories/Categories";
 import Header from "./components/header/Header";
 import Products from "./components/products/Products";
 
-
 function App() {
   return (
     <>
-     <Header/>
-     <div className="home px-6 flex justify-between gap-10">
-      <div className="categories flex-1 overflow-auto max-h-[calc(100vh)-_-112px] pb-64">
-        <Categories/>
-      </div>
-      <div className="products flex-[8]"><Products/></div>
+      <Header />
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories overflow-auto max-h-[calc(100vh)-_-112px] pb-64">
+          <Categories />
+        </div>
+        <div className="products flex-[8]">
+          <Products />
+        </div>
 
-      <div className="cart-totals"><div>cart totals</div></div>
-     </div>
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-ml-[24px] border">
+          <CartTotals />
+        </div>
+      </div>
     </>
   );
 }
