9fb77c7702612c9526809ffc7eb7d5c1fdcf7f17
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2023-06-01 14:03:57 +0300

    Register Page tamamlandı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import CartPage from "./pages/CartPage";
 import BillPage from "./pages/BillPage";
 import CustomerPage from "./pages/CustomerPage";
 import StatisticPage from "./pages/StatisticPage";
+import Register from "./pages/auth/Register";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/bills" element={<BillPage />} />
           <Route path="/customers" element={<CustomerPage />} />
           <Route path="/statistic" element={<StatisticPage />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
