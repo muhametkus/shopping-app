ff4bbfc9fcbf6de3530df90ad03fc2a78e90ff4f
Muhammet Kus <77937107+muhametkus@users.noreply.github.com>
2023-06-14 14:17:52 +0300

    express ve mongo db kurulumu yapıldı. Categroy model oluşturuldu.

---
+++ api/server.js
@@ -0,0 +1,31 @@
+const express=require("express");
+const mongoose=require("mongoose");
+const dotenv = require("dotenv");
+const app=express();
+const port=5000;
+
+dotenv.config();
+
+
+
+const connect=async ()=>{
+
+    try{
+        
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to mongoDB");
+    }
+    catch(error){
+        throw error;
+    }
+
+
+}
+
+
+app.get("/", (req,res)=>res.send("hello world"))
+
+app.listen(port, ()=>{
+    connect();
+    console.log(`Example app listening on port ${port}`);
+})
